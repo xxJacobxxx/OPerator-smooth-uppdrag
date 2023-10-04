065589a5943703991922822650d531ed474e7e68
Jacob <jacob.hansen@elev.ga.ntig.se>
2023-10-04 10:23:43 +0200

    Skapat folder och start

---
+++ src/App.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner tb = new Scanner(System.in);
+        System.out.println("pls skriv två heltal");
+        int tal1 = tb.nextInt();
+        int tal2 = tb.nextInt();
+        
+    }
+}
