28404a97a266fe845d943e6ca272f0cc93ffd1b0
Jacob <jacob.hansen@elev.ga.ntig.se>
2023-10-04 10:27:13 +0200

    Första delen klar

--- src/App.java
+++ src/App.java
@@ -6,6 +6,13 @@ public class App {
         System.out.println("pls skriv två heltal");
         int tal1 = tb.nextInt();
         int tal2 = tb.nextInt();
-        
+
+        System.out.println("Är första talet lika med andra talet?");
+        System.out.println(tal1==tal2);
+        System.out.println("Är första talet mindre än andra talet?");
+        System.out.println(tal1<=tal2);
+        System.out.println("Är första talet större?");
+        System.out.println(tal1>=tal2);
+
     }
 }
